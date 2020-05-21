3f33273d7663eb4b3a11ecee1e1da2f02b55575e
Andreea <andreea.pupe99@e-uvt.ro>
2020-05-22 02:18:54 +0300

    Fixed tiny bug

--- src/app/USER_RELATED/nav-toolbar-user/nav-toolbar-user.component.spec.ts
+++ src/app/USER_RELATED/nav-toolbar-user/nav-toolbar-user.component.spec.ts
@@ -1,25 +1,26 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing'
 
-import { NavToolbarUserComponent } from './nav-toolbar-user.component';
+import { NavToolbarUserComponent } from './nav-toolbar-user.component'
+import { MatMenuModule } from '@angular/material/menu'
 
 describe('NavToolbarUserComponent', () => {
-  let component: NavToolbarUserComponent;
-  let fixture: ComponentFixture<NavToolbarUserComponent>;
+  let component: NavToolbarUserComponent
+  let fixture: ComponentFixture<NavToolbarUserComponent>
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ NavToolbarUserComponent ]
-    })
-    .compileComponents();
-  }));
+      declarations: [NavToolbarUserComponent],
+      imports: [MatMenuModule],
+    }).compileComponents()
+  }))
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(NavToolbarUserComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    fixture = TestBed.createComponent(NavToolbarUserComponent)
+    component = fixture.componentInstance
+    fixture.detectChanges()
+  })
 
   it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
+    expect(component).toBeTruthy()
+  })
+})
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,9 +1,10 @@
 import { TestBed, async } from '@angular/core/testing'
 import { AppComponent } from './app.component'
+import { DashboardUserComponent } from './USER_RELATED/dashboard-user/dashboard-user.component'
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [AppComponent],
+      declarations: [AppComponent, DashboardUserComponent],
     }).compileComponents()
   }))
   it('should create the app', async(() => {
@@ -11,7 +12,7 @@ describe('AppComponent', () => {
     const app = fixture.debugElement.componentInstance
     expect(app).toBeTruthy()
   }))
-  it(`should have as title 'app'`, async(() => {
+  it(`should have as title 'Cert-Master'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent)
     const app = fixture.debugElement.componentInstance
     expect(app.title).toEqual('Cert-Master')
@@ -20,8 +21,5 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent)
     fixture.detectChanges()
     const compiled = fixture.debugElement.nativeElement
-    expect(compiled.querySelector('h1').textContent).toContain(
-      'Welcome to search-bar!'
-    )
   }))
 })
